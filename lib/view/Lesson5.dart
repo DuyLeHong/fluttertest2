f5fa7501bcb067af13c6bb61430869b261e55a76
Duy Le <lehongduy.bk@gmail.com>
2022-02-08 21:14:40 +0700

    Lesson 5: Images, Stack, ListView

--- lib/view/Lesson5.dart
+++ lib/view/Lesson5.dart
@@ -19,9 +19,14 @@ class MyApp extends StatelessWidget {
 }
 
 class MyPage extends StatelessWidget {
+  late final List<String> items;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    items = List<String>.generate(100, (i) => 'Phan tu thu $i');
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lesson 5'),
@@ -32,7 +37,6 @@ class MyPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
-
               children: [
                 Image.asset(
                   'Banner-Youtube-thumbnail-KH-ReactNative-4.png',
@@ -42,11 +46,25 @@ class MyPage extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
-                Image.network(
-                  'https://codefresher.vn/wp-content/uploads/2022/01/banner_2022_flutter.jpg',
-                  width: 200,
-                  height: 200,
-                )
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return Container(child: Text(items[index])); // neu dung widget tuy bien thi can dung them GestureDetector
+
+                      // return ListTile(
+                      //   title: Text(items[index]),
+                      //   subtitle: Text("Sub Title cua ${items[index]}"),
+                      //   onTap: () {
+                      //     String mess = items[index];
+                      //
+                      //     ScaffoldMessenger.of(context)
+                      //         .showSnackBar(SnackBar(content: Text(mess)));
+                      //   },
+                      // );
+                    },
+                  ),
+                ),
               ],
             )),
       ),
