fa734405b8bab01fe49192280ef0ffa21fd81858
Duy Le <lehongduy.bk@gmail.com>
2022-02-22 19:53:05 +0700

    Lesson 9: InheritedWidget

--- lib/view/Lesson9.dart
+++ lib/view/Lesson9.dart
@@ -11,23 +11,84 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Color frogColor = Colors.green;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Builder(builder: (BuildContext context) {
-          return FlatButton(
-            child: Text('show snackbar'),
-            color: Colors.pink,
-            onPressed: () {
-              // xử lý show snackbar khi click
-              final snackBar = SnackBar(content: Text('Nội dung text gì đó'));
-              Scaffold.of(context).showSnackBar(snackBar);
-            },
-          );
-        },),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Builder(
+              builder: (BuildContext context) {
+                return FlatButton(
+                  child: Text('show snackbar'),
+                  color: Colors.pink,
+                  onPressed: () {
+                    // xử lý show snackbar khi click
+                    final snackBar =
+                    SnackBar(content: Text('Nội dung text gì đó'));
+                    Scaffold.of(context).showSnackBar(snackBar);
+                  },
+                );
+              },
+            ),
+            FrogColor(
+              color: frogColor,
+              child: Builder(
+                builder: (BuildContext context) {
+                  return Text(
+                    'Hello Frog',
+                    style: TextStyle(color: FrogColor.of(context).color),
+                  );
+                },
+              ),
+            ),
+
+            ElevatedButton(onPressed: () {
+              setState(() {
+                if (frogColor == Colors.green) {
+                  frogColor = Colors.blueGrey;
+                } else {
+                  frogColor = Colors.green;
+                }
+              });
+            }, child: Text('Click Me!')),
+          ],
+        ),
       ),
     );
   }
+}
+
+class FrogColor extends InheritedWidget {
+  const FrogColor({
+    Key? key,
+    required this.color,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final Color color;
+
+  static FrogColor of(BuildContext context) {
+    final FrogColor? result =
+        context.dependOnInheritedWidgetOfExactType<FrogColor>();
+    assert(result != null, 'No FrogColor found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(FrogColor old) => color != old.color;
 }
