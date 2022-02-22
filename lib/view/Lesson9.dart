719c3c35ce81eb63b3235917b7ddfe27ea050a9f
Duy Le <lehongduy.bk@gmail.com>
2022-02-22 17:41:49 +0700

    Prepare for lesson 9

---
+++ lib/view/Lesson9.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          child: Text('show snackbar'),
+          color: Colors.pink,
+          onPressed: () {
+            // xử lý show snackbar khi click
+            final snackBar = SnackBar(content: Text('Nội dung text gì đó'));
+            Scaffold.of(context).showSnackBar(snackBar);
+          },
+        ),
+      ),
+    );
+  }
+}
